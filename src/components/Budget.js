95f502d1903211f6defdd5816824e0033595a498
chandramohan0 <thechandramohan01@gmail.com>
2023-09-17 14:32:19 -0400

    Components updated 2.0

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,14 +1,42 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, {useContext, useState} from 'react';
+import {AppContext} from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const {budget, expenses, currency} = useContext(AppContext);
+    const [budgetUpdated, setBudgetUpdated] = useState(budget);
+
+    const budgetLimit = 20000;
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const onBudgetUpdated = (e) => {
+        // Set the upper limit value to 20,000
+        if (budgetUpdated > (budgetLimit - 1)) {
+            alert("Budget cannot exceed " + currency + " " + budgetLimit);
+            setBudgetUpdated(budget);
+        } else if (budgetUpdated < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+            setBudgetUpdated(totalExpenses);
+        } else {
+            setBudgetUpdated(e.target.value)
+        }
+    }
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}
+                <input
+                    required='required'
+                    type='number'
+                    id='budget'
+                    value={budgetUpdated}
+                    style={{size: 10}}
+                    onChange={(event) => onBudgetUpdated(event)}>
+                </input>
+            </span>
         </div>
     );
 };
-
 export default Budget;
